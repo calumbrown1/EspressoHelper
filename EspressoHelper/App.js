96747753d75d1c86b070833ab328dada4856a449
CalumBrown <calumbrown1@hotmail.com>
2020-04-30 18:55:53 +0100

    More package stuff

--- EspressoHelper/App.js
+++ EspressoHelper/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+          title="Press me"
+          onPress={() => Alert.alert('Simple Button pressed', "You did it")}
+        />
     </View>
   );
 }
